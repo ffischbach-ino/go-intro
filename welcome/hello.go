dfd37d7f84dc99d5fd7e5cdfde0f5abd849538f7
Fynn Luca Fischbach <ffischbach@MacBook-Pro-von-Fynn.local>
2025-08-05 10:35:57 +0200

    introduced method to greet multiple people

--- welcome/hello.go
+++ welcome/hello.go
@@ -11,11 +11,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Fynn")
+	names := []string{"Hans", "Greg", "Leon"}
+	messages, err := greetings.GreetMultiple(names)
 
 	if err != nil {
 		log.Fatal((err))
 	}
 
-	fmt.Println(message)
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
