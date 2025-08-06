f37bd5faee94227bb5d9e22b934f9b8894aa66df
ffischbach-ino <fynn.fischbach@inovex.de>
2025-08-06 15:30:39 +0200

    added POST http listener on albums

--- web-service-gin/web-service.go
+++ web-service-gin/web-service.go
@@ -26,6 +26,7 @@ func getAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
