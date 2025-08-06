e256a8045c1fb283f64555a3d5d4ee8256855425
ffischbach-ino <fynn.fischbach@inovex.de>
2025-08-06 15:31:15 +0200

    added append logic for new albums

--- web-service-gin/web-service.go
+++ web-service-gin/web-service.go
@@ -23,6 +23,18 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
